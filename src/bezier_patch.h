86c996734ec93f2bf18df8cc74389cf418aa77cc
sushiS0N <blurr1tm@protonmail.com>
2026-01-21 16:23:50 +0000

    Working bezier patch, refactored code

---
+++ src/bezier_patch.h
@@ -0,0 +1,25 @@
+#pragma once
+
+// Bezier_patch header
+class BezierPatch
+{
+public:
+    BezierPatch(float *cp, int res);
+
+    // Explicitly delete copy operations
+    BezierPatch(const BezierPatch &) = delete;
+    BezierPatch &operator=(const BezierPatch &) = delete;
+
+    void evaluate(float u, float v, float *out_pos);
+    void generate_mesh();
+    void render();
+
+    ~BezierPatch();
+
+private:
+    float control_points[48];
+    int resolution;
+    sg_buffer vertex_buffer;
+    sg_buffer index_buffer;
+    int num_indices;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,21 @@
 #define SOKOL_IMPL
 #define SOKOL_D3D11
+
 #include "sokol_app.h"
-#include "sokol_log.h"
 #include "sokol_gfx.h"
 #include "sokol_glue.h"
+#include "sokol_log.h"
+
 #include "shader.h"
-#include <cstdio>
-#include <cmath>
-using namespace std;
+#include "bezier_patch.h"
 
-#define NUM_POINTS 100
+float cp[48] = {
+    -0.75f, -0.75f, 0.0f, -0.25f, -0.75f, 0.1f, 0.25f, -0.75f, 0.1f, 0.75f, -0.75f, 0.0f,
+    -0.75f, -0.25f, 0.1f, -0.25f, -0.25f, 0.3f, 0.25f, -0.25f, 0.3f, 0.75f, -0.25f, 0.1f,
+    -0.75f, 0.25f, 0.1f, -0.25f, 0.25f, 0.3f, 0.25f, 0.25f, 0.3f, 0.75f, 0.25f, 0.1f,
+    -0.75f, 0.75f, 0.0f, -0.25f, 0.75f, 0.1f, 0.25f, 0.75f, 0.1f, 0.75f, 0.75f, 0.0f};
 
-float vertices[NUM_POINTS * 7];
+BezierPatch *patch = new BezierPatch(cp, 10);
 
 static struct
 {
@@ -20,65 +24,17 @@ static struct
     sg_pass_action pass_action;
 } state;
 
-// Interpolations
-float lerp(float a, float b, float t)
-{
-    return a + (b - a) * t;
-}
-
-float cubic_bernstein(float t, float p0, float p1, float p2, float p3)
-{
-    float invT = 1 - t;
-    return (p0 * std::pow(invT, 3) +
-            p1 * 3 * std::pow(invT, 2) * t +
-            p2 * 3 * invT * std::pow(t, 2) +
-            p3 * std::pow(t, 3));
-}
-
 void frame()
 {
-    // Control points where the middle one oscilates sin(time)
-    float p0_x = -0.5f, p0_y = -0.5f;
-
-    float time = (float)sapp_frame_count() * 0.05f;
-    float p1_x = 0.0f, p1_y = 0.0f + sin(time) * 0.3f;
-
-    float p2_x = 0.7f, p2_y = -0.8f;
-    float p3_x = 0.9f, p3_y = 0.0f;
-
-    // Bernstein evaluation
-    for (int i = 0; i < NUM_POINTS; i++)
-    {
-        // normalize the t value
-        float t = (float)i / (float)(NUM_POINTS - 1);
-
-        // Bernstein points
-        float x = cubic_bernstein(t, p0_x, p1_x, p2_x, p3_x);
-        float y = cubic_bernstein(t, p0_y, p1_y, p2_y, p3_y);
-
-        // Fill buffer
-        int index = i * 7; // 7 floats per vertex
-        vertices[index + 0] = x;
-        vertices[index + 1] = y;
-        vertices[index + 2] = 0.0f;
-
-        // Color
-        vertices[index + 3] = 1.0f - t;
-        vertices[index + 4] = 0.0f;
-        vertices[index + 5] = t * sin(time);
-        vertices[index + 6] = 1.0f;
-    }
-
-    sg_update_buffer(state.bind.vertex_buffers[0], SG_RANGE(vertices));
-
     sg_pass pass = {};
     pass.action = state.pass_action;
     pass.swapchain = sglue_swapchain();
 
     sg_begin_pass(pass);
     sg_apply_pipeline(state.pip);
-    sg_apply_bindings(state.bind);
-    sg_draw(0, NUM_POINTS, 1);
+
+    patch->render();
+
     sg_end_pass();
     sg_commit();
 }
@@ -102,21 +58,7 @@ void init()
     desc.logger.func = slog_func;
     sg_setup(desc);
 
-    // float vertices[] = {
-    //     0.0f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f,
-    //     0.5f, -0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f,
-    //     -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f};
-    // // static buffer - simple triangle
-    // sg_buffer_desc buffer = {};
-    // buffer.data = SG_RANGE(vertices);
-    // buffer.label = "triangle_vertices";
-    // state.bind.vertex_buffers[0] = sg_make_buffer(buffer);
-
-    sg_buffer_desc buffer_desc = {};
-    buffer_desc.size = sizeof(vertices);
-    buffer_desc.usage.dynamic_update = true;
-    buffer_desc.label = "dynamic-vertices";
-    state.bind.vertex_buffers[0] = sg_make_buffer(buffer_desc);
+    patch->generate_mesh();
 
     // create shader
     sg_shader shd = sg_make_shader(shd_shader_desc(sg_query_backend()));
@@ -126,7 +68,8 @@ void init()
     pip_desc.shader = shd;
     pip_desc.layout.attrs[ATTR_shd_pos].format = SG_VERTEXFORMAT_FLOAT3;
     pip_desc.layout.attrs[ATTR_shd_color0].format = SG_VERTEXFORMAT_FLOAT4;
-    pip_desc.primitive_type = SG_PRIMITIVETYPE_LINE_STRIP;
+    pip_desc.primitive_type = SG_PRIMITIVETYPE_TRIANGLES;
+    pip_desc.index_type = SG_INDEXTYPE_UINT16;
     pip_desc.label = "triangle_pipeline";
 
     state.pip = sg_make_pipeline(pip_desc);
